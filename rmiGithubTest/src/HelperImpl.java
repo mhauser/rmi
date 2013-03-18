d65b9921baf8dcc093a70fbe1dd92425f364f56f
mhauser <mhauser@hsr.ch>
2013-03-18 17:17:32 +0100

    serializable

--- rmiGithubTest/src/HelperImpl.java
+++ rmiGithubTest/src/HelperImpl.java
@@ -1,5 +1,9 @@
+import java.io.Serializable;
 
-public class HelperImpl implements Helper {
+
+public class HelperImpl implements Helper, Serializable {
+
+	private static final long serialVersionUID = 3738299470507788173L;
 
 	@Override
 	public String decrypt(byte[] arg) {
